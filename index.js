107464a9810d010ef2e467c05ce5cfb124d354fd
Nicole kibe <nicolekibe822@gmail.com>
2025-09-08 20:02:03 +0300

    add the changes made to index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function expression
+const divide = function() {
+  return 2000 / 100;
+};
+
+// Arrow function (1 parameter)
+const square = x => x * x;
+
+// Arrow function (2 parameters)
+const add = (a, b) => a + b;
